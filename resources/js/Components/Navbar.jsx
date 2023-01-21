e534e75ae2361cb0792e914476ba452d066fa022
Xeliotropp <xeliotrop@gmail.com>
2023-01-21 11:34:53 +0200

    Making some changes to the navbar

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { ReactDOM } from "react";
 import { useState } from "react";
 
 import { useRef } from "react";
 import { Divide as Hamburger } from "hamburger-react";
 import {CgProfile} from "react-icons/cg";
 import '../../css/app.css'
-import { useNavigate } from "react-router";
 
 export default function Navbar() {
-  {/*fix this*/}
-  function goHome() {
-    return useNavigate("/");
-  }
+
   const NavRef = useRef();
   const [isOpen, setOpen] = useState(false)
   const ShowNavbar = () => {
@@ -37,18 +32,14 @@ export default function Navbar() {
         <button onClick={ShowNavbar}>  <Hamburger toggled={isOpen} toggle={setOpen} /></button>
 
         {/*logo*/}
-        <button onClick={goHome}>
+        <a href="/">
           <h2 className="logo">
             <span style={{ color: "#eb5160" }}>comp</span>stadium
           </h2>
-        </button>
-
-          {/* redirect to login/register page if the user doesn't have a profile else redirect the user to a page that show's their info and they can edit
-          their profile (ex. change their username, pfp, about me, email and password)
-          */}
-          <a href="/login">
-            <CgProfile/>
-          </a>
+        </a>
+        <a href="/login">
+          <CgProfile/>
+        </a> 
       </header>
     </>
   );
