fbd9503fd8e74c729d512f882d70a37aac84e546
Xeliotropp <xeliotrop@gmail.com>
2023-02-14 12:14:41 +0200

    Adding the front-end for project

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
@@ -18,18 +19,40 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
 
         patch(route('profile.update'));
     };
+    const passwordInput = useRef();
+    const currentPasswordInput = useRef();
+
+    const updatePassword = (e) => {
+        e.preventDefault();
+
+        put(route('password.update'), {
+            preserveScroll: true,
+            onSuccess: () => reset(),
+            onError: () => {
+                if (errors.password) {
+                    reset('password', 'password_confirmation');
+                    passwordInput.current.focus();
+                }
+
+                if (errors.current_password) {
+                    reset('current_password');
+                    currentPasswordInput.current.focus();
+                }
+            },
+        });
+    };
 
     return (
-        <section className={className}>
-            <header>
+        <div className={className} style={{ display: "flex", alignItems: "center", justifyContent: "center", flexFlow: "column", height: "80vh", width: "600px", gap: "225px", backgroundColor: "blue", borderRadius: "40px", margin: "24px 0px 0px     48px" }}>
+            <div style={{ display: "flex", top: 0, flexFlow: "column", alignItems: "center", justifyContent: "center" }}>
                 <h2 className="text-lg font-medium text-gray-900">Profile Information</h2>
 
                 <p className="mt-1 text-sm text-gray-600">
                     Update your account's profile information and email address.
                 </p>
-            </header>
 
-            <form onSubmit={submit} className="mt-6 space-y-6">
+            </div>
+            <form onSubmit={submit || updatePassword} className="mt-6 space-y-6" style={{ display: "flex", flexFlow: "column", height: "200px", gap: "1rem" }}>
                 <div>
                     <InputLabel for="name" value="Name" />
 
@@ -85,6 +108,64 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
                 )}
 
                 <div className="flex items-center gap-4">
+
+                    <Transition
+                        show={recentlySuccessful}
+                        enterFrom="opacity-0"
+                        leaveTo="opacity-0"
+                        className="transition ease-in-out"
+                    >
+                        <p className="text-sm text-gray-600">Saved.</p>
+                    </Transition>
+                </div>
+                <div>
+                    <InputLabel for="current_password" value="Current Password" />
+
+                    <TextInput
+                        id="current_password"
+                        ref={currentPasswordInput}
+                        value={data.current_password}
+                        handleChange={(e) => setData('current_password', e.target.value)}
+                        type="password"
+                        className="mt-1 block w-full"
+                        autoComplete="current-password"
+                    />
+
+                    <InputError message={errors.current_password} className="mt-2" />
+                </div>
+
+                <div>
+                    <InputLabel for="password" value="New Password" />
+
+                    <TextInput
+                        id="password"
+                        ref={passwordInput}
+                        value={data.password}
+                        handleChange={(e) => setData('password', e.target.value)}
+                        type="password"
+                        className="mt-1 block w-full"
+                        autoComplete="new-password"
+                    />
+
+                    <InputError message={errors.password} className="mt-2" />
+                </div>
+
+                <div>
+                    <InputLabel for="password_confirmation" value="Confirm Password" />
+
+                    <TextInput
+                        id="password_confirmation"
+                        value={data.password_confirmation}
+                        handleChange={(e) => setData('password_confirmation', e.target.value)}
+                        type="password"
+                        className="mt-1 block w-full"
+                        autoComplete="new-password"
+                    />
+
+                    <InputError message={errors.password_confirmation} className="mt-2" />
+                </div>
+
+                <div className="flex items-center gap-4">
                     <PrimaryButton processing={processing}>Save</PrimaryButton>
 
                     <Transition
@@ -97,6 +178,7 @@ export default function UpdateProfileInformation({ mustVerifyEmail, status, clas
                     </Transition>
                 </div>
             </form>
-        </section>
+
+        </div>
     );
 }
